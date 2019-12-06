1690fc7aba66ee438693512564af684e78cd1930
H.AOUASSAR <hssine36@gmail.com>
2019-12-06 15:43:26 +0100

    Updates

---
+++ precache-manifest.3cc26184bb9503c7882549f3b7a63acc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0121ae02a2af0cbc40d77a2dc775ebd",
+    "url": "/react-starter/index.html"
+  },
+  {
+    "revision": "ed5420f1d63b63cf6e17",
+    "url": "/react-starter/static/css/main.96e6b432.chunk.css"
+  },
+  {
+    "revision": "cbbce7163de049767e4c",
+    "url": "/react-starter/static/js/2.4d2affe9.chunk.js"
+  },
+  {
+    "revision": "ed5420f1d63b63cf6e17",
+    "url": "/react-starter/static/js/main.0ffe4c99.chunk.js"
+  },
+  {
+    "revision": "05e8b0b0aa366cd2b30d",
+    "url": "/react-starter/static/js/runtime-main.0d3ff233.js"
+  }
+]);
